72fb754634364b155d188911f1a9c071906cc975
Arie Knoester <arikstr@gmail.com>
2023-09-11 13:02:41 -0500

    Scoreboard class initialization.

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,18 @@
+from turtle import Turtle
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.goto(x=-280, y=255)
+        self.player_level = 1
+        self.display_level()
+
+    def display_level(self):
+        self.clear()
+        self.write(
+            f"Level: {self.player_level}", font=FONT)
